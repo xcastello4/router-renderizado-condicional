bc6369530b77d4b2f0ad20679de1ed7ca447aacb
Xavier Castello <xavier.castello@seidor.es>
2023-06-15 16:41:51 +0200

    add SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
 import './App.css'
+import { useEffect } from 'react';
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate (href) {
+  window.history.pushState({}, '', href)
+  // crear evento personalizado para que el navegador nos avise de que ha cambiado la ruta
+  const navigationEvent = new Event(NAVIGATION_EVENT)
+  window.dispatchEvent(navigationEvent)
+}
 
 function HomePage() {
   return (
     <>
       <h1>Home - Renderizado Condicional</h1>
       <p>Esta es una página de ejemplo para crear un React Router desde cero</p>
-      <a href='/about'>Ir a Sobre nosotros</a>
+      <button onClick={() => navigate('/about')}>Ir a Sobre nosotros</button>
     </>
   )
 }
@@ -19,13 +28,25 @@ function AboutPage() {
         <img src='https://pbs.twimg.com/profile_images/915346987234586624/5qruaoso_400x400.jpg' alt='Foto de Xavier'/>
         <p>¡Hola! Me llamo Xavier y estoy creando un renderizado condicional</p>
       </div>
-      <a href='/'>Ir a la home</a>
+      <button onClick={() => navigate('/')}>Ir a la home</button>
     </>
   )
 }
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  }, [])
 
   return (
     <main>

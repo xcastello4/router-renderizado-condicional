a95babc0e6887a1168c70abf04680ca8725fde7c
Xavier Castello <xavier.castello@seidor.es>
2023-06-15 16:32:56 +0200

    create MPA

---
+++ README.md
@@ -0,0 +1,5 @@
+# Crear un React Router desde cero
+
+- [x] Crear una forma de hacer SPAs (Single Page Applications)
+- [x] Crear una forma de hacer MPAs (Multiple Page Applications)
+- [ ] Poder navegar con el bóton de atrás
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function HomePage() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Home - Renderizado Condicional</h1>
+      <p>Esta es una página de ejemplo para crear un React Router desde cero</p>
+      <a href='/about'>Ir a Sobre nosotros</a>
     </>
+  )
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <div>
+        <img src='https://pbs.twimg.com/profile_images/915346987234586624/5qruaoso_400x400.jpg' alt='Foto de Xavier'/>
+        <p>¡Hola! Me llamo Xavier y estoy creando un renderizado condicional</p>
+      </div>
+      <a href='/'>Ir a la home</a>
+    </>
+  )
+}
+
+function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  return (
+    <main>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </main>
   )
 }
 
